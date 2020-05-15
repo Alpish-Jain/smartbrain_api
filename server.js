c00435659d64e288271a61699be47d63cc791003
Alpish Jain <jalpish@gmail.com>
2020-05-15 14:48:22 +0530

    fix

--- server.js
+++ server.js
@@ -10,10 +10,8 @@ const image=require('./controllers/image')
 const db=knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-shallow-30214',
-      user : 'postgres',
-      password : 'Alpish2000@',
-      database : 'smartbrain'
+      host : process.env.DATABASE_URL,
+      ssl:true,
     }
   });
 
